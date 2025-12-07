f2f639eb2910c2e7b0641a8b3e0cb64c4670f0db
Leto-Osen228 <leto-osen282@yandex.ru>
2025-12-07 16:55:18 +0300

    add opd/lab0/task2

---
+++ OPD/lab0/task2.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+chmod 513 ./bronzong4
+chmod a=ro=0 ./bronzong4/simisage
+chmod 440 ./bronzong4/hydreigon
+chmod 666 ./bronzong4/oshawott
+chmod 750 ./bronzong4/reuiclus
+chmod a=rwx ./bronzong4/lileep
+chmod u=,g=r,o=r ./bronzong4/sunflora
+
+chmod 045 ./klang6
+chmod 600 ./snover7
+chmod 050 ./staravia0
+
+chmod 711 ./tentacruel6
+chmod 050 ./tentacruel6/bedew
+chmod 312 ./tentacruel6/lilligant
+chmod u=rw,g=w,o=w ./tentacruel6/aggron
+
+chmod 571 ./trecho5
+chmod 307 ./tentacruel6/dusclops
+chmod 551 ./tentacruel6/kirlia
+chmod 045 ./tentacruel6/drapion
+chmod u=,g=rw,o=w ./tentacruel6/yanma
